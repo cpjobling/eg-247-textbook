69fd8d7369adf89209a05bb6a2376bfc52d4bcc4
Chris Jobling <cpjobling@gmail.com>
2019-01-07 23:38:13 +0000

    Trying static build into docs again

--- scripts/clean.py
+++ scripts/clean.py
@@ -6,7 +6,7 @@ import os.path as op
 path_root = op.join(op.dirname(op.abspath(__file__)), '..')
 
 paths = [op.join(path_root, '_site'),
-         op.join(path_root, '_build')]
+         op.join(path_root, 'docs')]
 for path in paths:
     print('Removing {}...'.format(path))
     sh.rmtree(path, ignore_errors=True)
