5bce4bc63bb4f25b58364e5383a753a024e91ca6
Chris Jobling <cpjobling@gmail.com>
2020-01-07 01:17:33 +0000

    mods to script content/setup/install_matlab_kernel.sh

--- content/setup/install_matlab_kernel.sh
+++ content/setup/install_matlab_kernel.sh
@@ -3,15 +3,16 @@
 MATLAB_HOME=/Applications/MATLAB_R2019a.app
 EG_247=$HOME/dev/eg-247-textbook
 
+# Set up for jupyter with rise and jupyter-book
 cd $EG_247
 conda create -n eg-247-textbook python=3.7 anaconda
 conda activate eg-247-textbook
-
 conda install -c conda-forge rise
+pip install jupyter-book
 
-cd $MATLAB_HOME%\extern\engines\python
-python setup.py install
+# Set up Python-MATLAB bridge and matlab_kernel
+python $MATLAB_HOME/extern/engines/python/setup.py install
+pip install matlab_kernel
 
-cd $EG_247
-pip instal matlab_kernel
+
 
