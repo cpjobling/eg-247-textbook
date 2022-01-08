4dd80c37944e1afc8e8e7b3bfdc85db285566d1f
cpjobling <cpjobling@gpjobling.net>
2022-01-08 20:07:25 +0000

    Update documentation

--- setup/install_matlab_kernel.sh
+++ setup/install_matlab_kernel.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-MATLAB_HOME=/Applications/MATLAB_R2020b.app
+MATLAB_HOME=/Applications/MATLAB_R2021b.app
 EG_247=$HOME/code/src/github.com/cpjobling/eg-247-textbook
 
 # Set up for jupyter with rise and jupyter-book
 cd $EG_247
+conda delete eg-247-textbook
 conda update conda
-conda create -n eg-247-textbook python=3.7 anaconda
+conda create -n eg-247-textbook python=3.9 anaconda
 conda activate eg-247-textbook
 conda install -c conda-forge rise
 pip install -U jupyter-book
+pip install -U jupytext
 
 # Set up Python-MATLAB bridge and matlab_kernel
 cd $MATLAB_HOME/extern/engines/python
