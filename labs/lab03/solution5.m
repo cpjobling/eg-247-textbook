ba28c58b1835a9ef83c9a5281f5a5c7b5f8f5383
cpjobling <cpjobling@gpjobling.net>
2023-03-02 14:52:44 +0000

    Update documentation

--- labs/lab03/solution5.m
+++ labs/lab03/solution5.m
@@ -1,4 +1,5 @@
 %% solution5 - Verification of Solution for Example 5 from notes.
+
 syms s;
 
 z1 = 13*s + 8/s;
@@ -8,5 +9,3 @@ z3 = 20 + 16/s;
 z = z1 + z2 * z3 /(z2 + z3)
 %%
 z10 = simplify(z)
-%%
-pretty(z10)
